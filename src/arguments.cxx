3df4bd1496b7b17c4a23b3f72f5a1f3fda06802f
torph <73564623+Torphedo@users.noreply.github.com>
2025-05-29 23:52:07 -0400

    Fix broken argument parsing

--- src/arguments.cxx
+++ src/arguments.cxx
@@ -3,18 +3,20 @@
 #include <cassert>
 #include <common/int.h>
 
-static const char* const flags[] = {
+static const char* const flags[ARG_ENUM_MAX] = {
     "--db",
 };
 
 arguments::arguments(int argc, char** argv) {
     for (u32 i = 0; i < argc; i++) {
         for (u32 j = 0; j < ARG_ENUM_MAX; j++) {
-            seen_flags[j] = strcmp(argv[i], flags[j]) == 0;
-            if (seen_flags[j]) {
-                // We found an argument flag!
-                assert(i + 1 < argc && "A flag had no value!"); // Bounds check
-                // Next argument is the value
+            // Check every argument against every known flag
+            if (strcmp(argv[i], flags[j]) == 0) {
+                // If the flag was the last argument, there's no string value to get out...
+                assert((i + 1) < (argc - 1) && "One of the flags is missing a value!");
+
+                // Everything's good, mark that we found the flag and store it
+                seen_flags[j] = true;
                 flag_values[j] = argv[i + 1];
                 i++;
                 first_non_flag = i;
