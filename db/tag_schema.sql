0939f42cfd6d39152ccbb862d401adeb3b991e12
torph <73564623+Torphedo@users.noreply.github.com>
2025-06-05 20:10:02 -0400

    Use hash instead of auto-inc ID for tags

--- db/tag_schema.sql
+++ db/tag_schema.sql
@@ -1,6 +1,5 @@
 -- Schema for the table of all tags
 CREATE TABLE IF NOT EXISTS "tags" (
     "tag" TEXT NOT NULL UNIQUE,
-    "id"  INTEGER NOT NULL UNIQUE,
-    PRIMARY KEY("id" AUTOINCREMENT)
-) STRICT;
+    "hash"  INTEGER NOT NULL UNIQUE
+)STRICT;
