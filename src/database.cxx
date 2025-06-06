30884604ceb0e5824ecefc5952f1c2a7b9f5e9f2
torph <73564623+Torphedo@users.noreply.github.com>
2025-06-05 20:29:52 -0400

    Inline import_single_file()

--- src/database.cxx
+++ src/database.cxx
@@ -93,107 +93,91 @@ void song_record::insert_sql(std::string& out) const noexcept {
     out.append(sqlbuf);
 }
 
-// TODO: Make this take a const u8*. Requires changes in other areas, and maybe a read-only variant of vfile.
-bool import_single_file(const char* files_dir, u8* mp3, u32 size, std::string& sql_out) {
-    // Collect metadata to fill out the record.
-    // This structure has pointers into the MP3, so we can't free it yet
-    const song_record song(mp3, size);
-
-    // Buffer for the path where the file will be copied to
-    // [files_dir]/[hash].mp3
-    char pathbuf[512] = {0};
-
-    // Make sure the path will fit in our static sized buffer.
-    // hash -> [up to] 10 chars, extension -> 4 chars, dirsep -> 1 char
-    assert(ARRAY_SIZE(pathbuf) > (strlen(files_dir) + 10 + 4 + 1) && "Path is too long to fit!");
-
-    // TODO: Update bobtail with a function to grab the file extension, and only
-    // use this default when there's no file extension.
-    const char* extension = ".mp3";
-    snprintf(pathbuf, ARRAY_SIZE(pathbuf), "%s%c%d%s", files_dir, PLATFORM_DIRSEP, song.crc32, extension);
-
-    // Copy imported file into the database folder
-    // std::filesystem::copy_file(path, pathbuf);
-
-    // Generate INSERT statement from metadata
-    song.insert_sql(sql_out);
-
-    return true;
-}
-
-bool import_single_file(const char* path, const char* files_dir, std::string& sql_out) {
-
-    assert(path_has_extension(path, ".mp3") && "Non-MP3 files aren't supported yet");
-
-    if (!file_exists(path)) {
-        return false;
-    }
-
-    const u32 size = file_size(path);
-    u8* mp3 = file_load(path);
-    if (mp3 == nullptr) {
-        LOG_MSG(error, "Failed to load \"%s\"\n", path);
-        return false;
-    }
-
-    const bool result = import_single_file(files_dir, mp3, size, sql_out);
-    free(mp3);
-
-    return result;
-}
-
 bool import_many_files(const char** paths, u32 num_paths, const char* files_dir, sqlite3* db) {
     bool result = true;
     std::string sql = "BEGIN TRANSACTION;\n";
-    u32 num_songs = 0;
-    float sqlgen_time = 0.0f;
+    u32 imported_songs = 0;
+    float sqlgen_time = 0.0f; // Elapsed runtime for importing and generating SQL INSERTs
     {
         const scope_timer generator_timer(sqlgen_time);
         std::vector<u8> mp3_buf(5 * 1024 * 1024);
+        // Indices of all paths that were found to already be in the database
+        std::vector<u32> import_conflicts;
         for (u32 i = 0; i < num_paths; i++) {
-            if (!file_exists(paths[i])) {
-                LOG_MSG(debug, "Skipping \"%s\" (it doesn't exist)\n", paths[i]);
-                continue;
-            }
+            // Early exit for simple errors
             if (!path_has_extension(paths[i], ".mp3")) {
                 LOG_MSG(debug, "Skipping \"%s\" (not an MP3)\n", paths[i]);
                 continue;
             }
+            if (!file_exists(paths[i])) {
+                LOG_MSG(debug, "Skipping \"%s\" (it doesn't exist)\n", paths[i]);
+                continue;
+            }
 
+            // Load the file
             const u32 size = file_size(paths[i]);
             if (size > mp3_buf.capacity()) {
                 mp3_buf.reserve(size + 1);
             }
             file_load_existing(paths[i], mp3_buf.data(), size);
 
-            import_single_file(files_dir, mp3_buf.data(), size, sql);
-            num_songs++;
+            // Extract metadata & hash the file
+            const song_record song(mp3_buf.data(), size);
+
+            // Copy file into database folder with hash for its name
+
+            // Buffer for the path where the file will be copied to
+            // [files_dir]/[hash].mp3
+            char pathbuf[512] = {0};
+
+            // Make sure the path will fit in our static sized buffer.
+            // hash -> [up to] 10 chars, extension -> 4 chars, dirsep -> 1 char
+            assert(ARRAY_SIZE(pathbuf) > (strlen(files_dir) + 10 + 4 + 1) && "Path is too long to fit!");
+
+            // TODO: Update bobtail with a function to grab the file extension, and only
+            // use this default when there's no file extension.
+            const char* extension = ".mp3";
+            snprintf(pathbuf, ARRAY_SIZE(pathbuf), "%s%c%d%s", files_dir, PLATFORM_DIRSEP, song.crc32, extension);
+
+            if (file_exists(pathbuf)) {
+                // File with this hash already exists in the database. Either a
+                // duplicate (very likely) or a hash conflict.
+                import_conflicts.push_back(i);
+            } else {
+                // Copy file into the database folder for import
+                std::filesystem::copy_file(paths[i], pathbuf);
+
+                // Generate INSERT statement
+                song.insert_sql(sql);
+                imported_songs++;
+            }
         }
     }
     sql.append("\nCOMMIT;\n");
 
+    // TODO: Try to figure out if any of the failed imports are real hash conflicts (not duplicates)
+
     float sqlexec_time = 0.0f;
-    if (num_songs > 0) {
-        LOG_MSG(info, "Finished generating SQL code (%d inserts) in %.3fms!\n", num_songs, sqlgen_time);
-
-        char* errmsg = nullptr;
-        int result = SQLITE_OK;
-        { // Scope to control the timer
-            const scope_timer sql_timer(sqlexec_time);
-            result = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errmsg);
-        }
-        if (result != SQLITE_OK) {
-            result = false;
-            if (errmsg) {
-                LOG_MSG(error, "SQLite error: %s\n", errmsg);
-            }
-        }
-
-        LOG_MSG(debug, "SQL compile/execute finished in %.3fms\n", sqlexec_time);
-    } else {
+    if (imported_songs == 0) {
         LOG_MSG(info, "It doesn't seem like you provided any MP3 files.\n");
-        result = false;
+        return false;
     }
+    LOG_MSG(info, "Finished generating SQL code (%d inserts) in %.3fms!\n", imported_songs, sqlgen_time);
+
+    char* errmsg = nullptr;
+    int sql_result = SQLITE_OK;
+    { // Scope to control the timer
+        const scope_timer sql_timer(sqlexec_time);
+        result = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errmsg);
+    }
+    if (sql_result != SQLITE_OK) {
+        result = false;
+        if (errmsg) {
+            LOG_MSG(error, "SQLite error: %s\n", errmsg);
+        }
+    }
+
+    LOG_MSG(debug, "SQL compile/execute finished in %.3fms\n", sqlexec_time);
 
     return result;
 }
