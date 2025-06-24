aeeb7d558779bf796e7f548ac94f6699945b246b
torph <73564623+Torphedo@users.noreply.github.com>
2025-06-23 23:29:15 -0400

    Fix single negated tag search

--- src/tags.cxx
+++ src/tags.cxx
@@ -88,19 +88,19 @@ void search_many_tags_and(const char* const* tags, u32 num_tags, std::string& sq
     // parent/child table tells us that all results for "mos def" will be
     // included in "rap".
 
-    // Tag search gives us a list of IDs, but we want a query to pull out the actual song entries
-    sql_out.append("SELECT * FROM songs s WHERE s.hash IN (\n");
+    // Tag search gives us hashes, so we use a compound SELECT to pull out full rows.
+    // SELECT-all at the end lets us put a compound keyword in front of all future SELECTs.
+    // This is both simpler and makes single negated tags work correctly.
+    sql_out.append("SELECT * FROM songs s WHERE s.hash IN (SELECT hash FROM songs\n");
 
     for (u32 i = 0; i < num_tags; i++) {
         // If the tag starts with "-", we interpret that as "AND NOT [tag]".
         const bool invert_tag = tags[i][0] == '-';
 
-        // Add compound operator before each SELECT, except the first one.
+        // Add compound operator before each SELECT.
         // EXCEPT implements the "NOT" behaviour we want if the tag is inverted.
         const char* compounder = invert_tag ? "\nEXCEPT " : "\nINTERSECT ";
-        if (i > 0) {
-            sql_out.append(compounder);
-        }
+        sql_out.append(compounder);
 
         // Increment tag pointer to skip the "-" if needed
         search_tag(tags[i] + invert_tag, sql_out, false);
