bb65ba081bb4b0280bc3234e5f726ea7eeba918a
torph <73564623+Torphedo@users.noreply.github.com>
2025-07-01 22:55:22 -0400

    Make missing CRC32 extension a hard error

--- src/main.cxx
+++ src/main.cxx
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
     enable_win_ansi();
 
     const arguments args(argc, argv);
+    int result = EXIT_SUCCESS;
 
     // Default DB path
     const char* db_path = "../db/blackbook.db3";
@@ -38,11 +39,12 @@ int main(int argc, char** argv) {
     sqlite3_initialize();
     sqlite3* db = nullptr;
     const int res = sqlite3_open(db_path, &db);
+    char* errmsg = nullptr;
     if (res != SQLITE_OK) {
         const char* msg = sqlite3_errmsg(db);
         LOG_MSG(error, "Failed to open database \"%s\" (reason: \"%s\")!\n", db_path, msg);
-        sqlite3_close(db);
-        return 1;
+        result = EXIT_FAILURE;
+        goto exit;
     }
     LOG_MSG(info, "Opened database \"%s\"\n", db_path);
 
@@ -51,30 +53,31 @@ int main(int argc, char** argv) {
 
     // Enable extension loading (from C only, not SQL) and try to load CRC32 module
     sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION, 1, nullptr);
-    char* errmsg = nullptr;
     if (sqlite3_load_extension(db, "./libsqlite_crc32", nullptr, &errmsg) != SQLITE_OK) {
         if (errmsg) {
             LOG_MSG(error, "Unabled to load CRC32 extension because: %s\n", errmsg);
         }
+        result = EXIT_FAILURE;
+        goto exit;
     } else {
         LOG_MSG(info, "Successfully loaded CRC32 extension.\n");
     }
 
-
-    int result = EXIT_SUCCESS;
     if (args.cli_mode) {
         result = cli_main(args, db, db_path, db_files_folder);
     } else {
         nativegui gui(db, db_files_folder.c_str());
         if (!gui.initialized) {
             LOG_MSG(error, "Failed to start up the GUI!\n");
-            return EXIT_FAILURE;
+            result = EXIT_FAILURE;
+            goto exit;
         }
 
         // We invert the return value since exit code 0 == false == EXIT_SUCCESS
         result = !gui_loop(nativegui::gui_main_static, &gui);
     }
 
+exit:
     sqlite3_close(db);
     sqlite3_shutdown();
     return result;
