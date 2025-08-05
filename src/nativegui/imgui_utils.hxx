b0f67a7ace59d755d508814f0dfbffc5098941a3
torph <73564623+Torphedo@users.noreply.github.com>
2025-08-05 18:35:48 -0400

    GUI: Fix missing include

--- src/nativegui/imgui_utils.hxx
+++ src/nativegui/imgui_utils.hxx
@@ -1,6 +1,7 @@
 #pragma once
 #include <imgui.h>
 #include "expression.hxx"
+#include <blackbook_core.hxx>
 
 namespace ImGui {
     bool InputTagAutocompleted(const char* label, const char* hint, ImGuiInputTextFlags flags, tag_autocomplete& tac, blackbook_core& core);
