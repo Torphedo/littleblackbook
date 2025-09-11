2ecc5df864e89a22477bb28c2da9ad74f95b129d
torph <73564623+Torphedo@users.noreply.github.com>
2025-09-10 21:21:17 -0400

    GUI: Don't use nearest neighbor in minification filter

--- src/nativegui/thumbnails.cxx
+++ src/nativegui/thumbnails.cxx
@@ -23,7 +23,7 @@ void update_gl_tex(texture img, gl_obj gl_tex) {
     // Wrapping & filtering settings
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     GLint res = (img.height * img.width);
